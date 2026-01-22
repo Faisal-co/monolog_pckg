9307779c1d0997e95142797e6b3eb6b27344e914
Faisal <softtechrana05@gmail.com>
2026-01-22 18:11:30 +0500

    src User class added

--- namespaces.php
+++ namespaces.php
@@ -2,7 +2,9 @@
 require __DIR__.'/controllers/User.php';
 require __DIR__.'/models/User.php';
 require __DIR__.'/views/User.php';
+require __DIR__.'/src/User.php';
 
 $objCont = new Cont\Usser\User();
 $objMod = new Mod\Usser\User();
 $objVew = new Vew\Usser\To\User();
+$objVew = new Srcc\Usser\User();
---
+++ src/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace Srcc\Usser;
+
+class User{
+    public function __construct(){
+        echo "This is Src User class";
+    }
+}
